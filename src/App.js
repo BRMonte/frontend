56884eafd2aca652c8f63bb2db9b164fb840a7fb
Bruno Montebello <brunormontebello@hotmail.com>
2020-10-25 20:28:07 +0000

    Create first component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* todo componente precisa ter o REACT importado */
+
+import React from 'react';
+
+function App() {
+  return <h1>Hello GoStack</h1>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App'; // importei o componente App do arquivo App.js
+
+// JSX: HTML dentro do JS (JavaScript XML)
+
+render(<App/>, document.getElementById('app'));
