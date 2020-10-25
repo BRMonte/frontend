82e1ec8fb15e0675138e685b8ca796250638171a
Bruno Montebello <brunormontebello@hotmail.com>
2020-10-25 17:42:11 +0000

    Config webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+//webpack: p cada tipo de arquivo (.js, .css, .png) fará uma conversao específica. Automatiza esse processo
+
+const path = require('path'); //convoca a lib path do node js
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'), //aqui determinamos qual e o arquivo de entrada da nossa app a ser convertido
+  output: { //aqui determinamos qual arquivo vai ser gerado depois de ser convertido
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+};
