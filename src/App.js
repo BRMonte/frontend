a2d30ea1d4c5fe22a5b9d6e9331202a41b5a245c
Bruno Montebello <brunormontebello@hotmail.com>
2020-10-25 20:50:42 +0000

    Fragments for duplicating components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-/* todo componente precisa ter o REACT importado */
+/* todo componente precisa ter o REACT importado
+- se quero ter elementos em sequencia, preciso ter algo que os envolva. Por isso a div em volta dos Headers
+- mas para nao ter q envolverdentro de uma div, o q pode gerar confusão na hora de estilizar etc,
+- usa-se o conceito de FRAGMENT, q nd mais é q <> </> ao redor dos componentes dobrados
+
+*/
 
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
-  return <h1>Hello GoStack</h1>;
+  return (
+    <>
+      <Header/>
+      <Header/>
+    </>
+  );
 }
 
 export default App;
