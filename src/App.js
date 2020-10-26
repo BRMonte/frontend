6fee9bdfd149142f5b34679370c3a94402da96b3
Bruno Montebello <brunormontebello@hotmail.com>
2020-10-26 23:46:45 +0000

    Imutabilidade

--- src/App.js
+++ src/App.js
@@ -6,17 +6,34 @@
 a- componente:
 b- propriedade: infos que podem ser passados do componente pai para o filho.
 c- Estado:
+d- imutabilidade: n se pode alterar formato de variaveis. Se quiser alterar, tera que recriar
+
+key: propriedade que veicula a informação unica. como o nome, titulo ou id de algo
 */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['Dev app', 'Front end web']); //useState retorna um array c 2 posiçoes. 1- vsriavel c seu valor inicial; 2- função p atualizar esse valor
+
+  function handleAddProject() {
+    // projects.push(`Novo projeto ${Date.now()}`);
+    setProjects([...projects, `Novo projeto ${Date.now()}`]); //deverá sguir a regra de imutabilidade e recriar o array de projetos ao inves der altera-lo, como faz o metodo push acima
+    // ...projects copia o array projetcs
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Hompeage"/>
       <Header title="Projects"/>
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Add projeto</button>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default function Header(props) { // como parametro da função Header apenas nao passadas as propriedades/atributo dos componentes
+export default function Header( {title}) { // como parametro da função Header apenas nao passadas as propriedades/atributo dos componentes
   return (
     <header>
-      <h1>ReactJS</h1>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
     </header>
   );
 }
